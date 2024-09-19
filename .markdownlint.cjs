e84a26dc2b3c8e097d0924fcbae7895c5b1f740f
Sergey Kupletsky <s.kupletsky@gmail.com>
2024-09-19 10:25:19 +0200

    chore: allow img in markdown files

--- .markdownlint.cjs
+++ .markdownlint.cjs
@@ -13,10 +13,17 @@ module.exports = {
   },
   'MD013': {
     'line_length': 120,
+    'ignore_code_blocks': true,
+    'ignore_urls': true,
     'code_blocks': false,
     'tables': false,
   },
   'MD024': {
     'siblings_only': true,
   },
+  'MD033': {
+     'allowed_elements': [
+        'img',
+     ]
+  }
 };
